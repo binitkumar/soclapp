faf26bcf8cc476c50b4d9abe64148c1a33f167cb
Adam Nottingham <adam.nottingham@gmail.com>
2014-07-08 17:42:08 -0600

    first commit

---
+++ assets/js/controllers.js
@@ -0,0 +1,35 @@
+angular.module('LikeSorter', ['ui.bootstrap']); v
+
+// Controller for pulling soundcloud data
+
+function TopListCtrl($scope, $http, $modal) {
+    $scope.getData = function (sc_user) {
+        SC.get('/users/' + sc_user + '/favorites', {
+            limit: 200
+        }, function (tracks) {
+            $scope.$apply(function () {
+                if (Object.getOwnPropertyNames(tracks).length > 1) {
+                    $scope.likes = tracks;
+                    $scope.reverse = true;
+                    $scope.sortField = 'favoritings_count';
+                    $scope.sc_user = sc_user;
+                } else {
+                    alert("That user has 0 Soundcloud likes or does not exist. Double check the username.")
+                }
+            })
+        });
+    }
+
+    //$scope.sortField = 'like.favoritings_count';
+    $scope.getData('galvanix');
+
+    // Opens the About popup info modal
+    $scope.openpopup = function () {
+        $modal.open({
+            templateUrl: 'myModalContent.html',
+            backdrop: true,
+            windowClass: 'modal'
+        });
+    };
+
+}
